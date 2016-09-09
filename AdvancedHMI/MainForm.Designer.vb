11ac3d0b89df04a06adacebf558b93ae1e95c745
Tempered Enterprises <jason.jensen@temperedenterprises.com>
2016-09-09 09:52:17 -0500

    Initial
    Initial

---
+++ AdvancedHMI/MainForm.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    '   <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    ' <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.QuickStartLabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ForceItemsIntoToolBox1 = New MfgControl.AdvancedHMI.Drivers.ForceItemsIntoToolbox()
+        Me.LicenseNote = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(12, 521)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 32)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "For Development Source Code Visit" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://www.advancedhmi.com"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'QuickStartLabel
+        '
+        Me.QuickStartLabel.AutoSize = True
+        Me.QuickStartLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.QuickStartLabel.ForeColor = System.Drawing.Color.White
+        Me.QuickStartLabel.Location = New System.Drawing.Point(12, 9)
+        Me.QuickStartLabel.Name = "QuickStartLabel"
+        Me.QuickStartLabel.Size = New System.Drawing.Size(273, 104)
+        Me.QuickStartLabel.TabIndex = 38
+        Me.QuickStartLabel.Text = resources.GetString("QuickStartLabel.Text")
+        Me.QuickStartLabel.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(450, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(322, 47)
+        Me.PictureBox1.TabIndex = 42
+        Me.PictureBox1.TabStop = False
+        '
+        'LicenseNote
+        '
+        Me.LicenseNote.AutoSize = True
+        Me.LicenseNote.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LicenseNote.Location = New System.Drawing.Point(19, 265)
+        Me.LicenseNote.Name = "LicenseNote"
+        Me.LicenseNote.Size = New System.Drawing.Size(746, 32)
+        Me.LicenseNote.TabIndex = 48
+        Me.LicenseNote.Text = "By Using This Software You Agree to the UsageAndLicense.txt" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "AdvancedHMI is licen" & _
+    "sed under a GPL model which means you must pass on the full source to the end us" & _
+    "er."
+        Me.LicenseNote.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.LicenseNote.Visible = False
+        '
+        'MainForm
+        '
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(784, 562)
+        Me.Controls.Add(Me.LicenseNote)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.QuickStartLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.KeyPreview = True
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "AdvancedHMI v3.99p"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents QuickStartLabel As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents DF1ComWF1 As AdvancedHMIDrivers.SerialDF1forSLCMicroCom
+    Friend WithEvents ForceItemsIntoToolBox1 As MfgControl.AdvancedHMI.Drivers.ForceItemsIntoToolbox
+    Friend WithEvents LicenseNote As System.Windows.Forms.Label
+End Class
---
+++ AdvancedHMI/MainForm.vb
@@ -0,0 +1,15 @@
+﻿Public Class MainForm
+    '*******************************************************************************
+    '* Stop polling when the form is not visible in order to reduce communications
+    '* Copy this section of code to every new form created
+    '*******************************************************************************
+    Private NotFirstShow As Boolean
+    Private Sub Form_VisibleChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.VisibleChanged
+        '* Do not start comms on first show in case it was set to disable in design mode
+        If NotFirstShow Then
+            AdvancedHMIDrivers.Utilities.StopComsOnHidden(components, Me)
+        Else
+            NotFirstShow = True
+        End If
+    End Sub
+End Class
